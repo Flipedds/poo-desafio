ddb30eaa41b0bf5bc3be3b16e058a0f52217f355
Filipe André <flipeandre12@gmail.com>
2023-10-18 20:46:29 -0300

    chore: utilizando classe iphone

--- application/Program.cs
+++ application/Program.cs
@@ -2,3 +2,6 @@
 
 Nokia cel = new("59684968", "Asha", "2898439323", 8);
 cel.InstalarAplicativo("facebook");
+
+Iphone iphone = new("23424243", "12", "8312938293", 256);
+iphone.InstalarAplicativo("Youtube");
